519a05d5118f5f4c56ddda9c155cb2fba4204067
Ashutosh <Ashutosh>
2024-03-04 08:54:12 +0530

    Unsubsribed from the observables to avoid memory leaks when the component gets destroyed

--- Angular-Component-Communication/src/app/products/product-shell/product-shell-detail.component.ts
+++ Angular-Component-Communication/src/app/products/product-shell/product-shell-detail.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { IProduct } from '../product';
-import { timer } from 'rxjs';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
     selector: 'pm-product-shell-detail',
     templateUrl: './product-shell-detail.component.html'
 })
-export class ProductShellDetailComponent implements OnInit {
+export class ProductShellDetailComponent implements OnInit, OnDestroy {
   pageTitle = 'Product Detail';
   // Need to handle null to allow for no selected product.
   errorMessage = '';
 
   product: IProduct | null = null;
+
+  sub!: Subscription;
 
   // get prod(): IProduct | null {
   //   return this.productService.currentProduct;
@@ -23,9 +25,13 @@ export class ProductShellDetailComponent implements OnInit {
     ngOnInit() {
 
       // timer(0, 1000).subscribe(data => console.log(this.prod));
-      this.productService.selectedProductChanges$.subscribe(
+      this.sub = this.productService.selectedProductChanges$.subscribe(
         selectedProduct => this.product = selectedProduct
       );
+    }
+
+    ngOnDestroy(): void {
+      this.sub.unsubscribe();
     }
 
 }
--- Angular-Component-Communication/src/app/products/product-shell/product-shell-list.component.ts
+++ Angular-Component-Communication/src/app/products/product-shell/product-shell-list.component.ts
@@ -1,28 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { IProduct } from '../product';
 import { ProductService } from '../product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'pm-product-shell-list',
   templateUrl: './product-shell-list.component.html'
 })
-export class ProductShellListComponent implements OnInit {
+export class ProductShellListComponent implements OnInit, OnDestroy {
   pageTitle = 'Products';
   products: IProduct[] = [];
   errorMessage = '';
+  sub1!: Subscription;
+  sub2!: Subscription;
 
   selectedProduct: IProduct | null = null;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.selectedProductChanges$.subscribe(product => this.selectedProduct = product);
-    this.productService.getProducts().subscribe({
+    this.sub1 = this.productService.selectedProductChanges$.subscribe(product => this.selectedProduct = product);
+    this.sub2 = this.productService.getProducts().subscribe({
       next: products => this.products = products,
       error: err => this.errorMessage = err
     });
+  }
 
+  ngOnDestroy(): void {
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
   }
 
   onSelected(product: IProduct) {
