9a332248bf73afea2c66c8f73fc9da3c966b10a0
Ashutosh <Ashutosh>
2024-03-04 06:23:00 +0530

    Using timer for change detection

--- Angular-Component-Communication/src/app/products/product-shell/product-shell-detail.component.ts
+++ Angular-Component-Communication/src/app/products/product-shell/product-shell-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { IProduct } from '../product';
+import { timer } from 'rxjs';
 
 @Component({
     selector: 'pm-product-shell-detail',
@@ -11,13 +12,15 @@ export class ProductShellDetailComponent implements OnInit {
   // Need to handle null to allow for no selected product.
   errorMessage = '';
 
-  get product(): IProduct | null {
+  get prod(): IProduct | null {
     return this.productService.currentProduct;
   }
 
     constructor(private productService: ProductService) { }
 
     ngOnInit() {
+
+      timer(0, 1000).subscribe(data => console.log(this.prod));
     }
 
 }
