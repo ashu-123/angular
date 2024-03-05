995e57d43cf67b05662b110fab021336ec95f787
Ashutosh <Ashutosh>
2024-03-05 15:50:37 +0530

    always keep class members in advised sequence

--- Angular-Best-Practices/src/app/users/register.component.ts
+++ Angular-Best-Practices/src/app/users/register.component.ts
@@ -7,41 +7,45 @@ import { Component } from "@angular/core";
 @Component({
     styleUrls: ['./register.component.css'],
     templateUrl: './register.component.html'
-  })
-  
-  export class RegisterComponent {
+})
+
+export class RegisterComponent {
     registerForm: FormGroup;
     firstName: FormControl;
     lastName: FormControl;
     email: FormControl;
     password: FormControl;
     saving: boolean = false;
-  
+
     constructor(private router: Router, private userRepository: UserRepositoryService) {
-      this.firstName = new FormControl('', Validators.required);
-      this.lastName = new FormControl('', Validators.required);
-      this.email = new FormControl('', Validators.required);
-      this.password = new FormControl('', Validators.required);
-  
-      this.registerForm = new FormGroup({
-        firstName: this.firstName,
-        lastName: this.lastName,
-        email: this.email,
-        password: this.password
-      });
-    }
-  
-  
-    registerUser(user: IUser) {
-      this.saving = true;
-      this.userRepository.saveUser(user)
-        .subscribe({
-          error: () => this.saving = false,
-          complete: () => this.router.navigate(['/catalog'])
+        this.firstName = new FormControl('', Validators.required);
+        this.lastName = new FormControl('', Validators.required);
+        this.email = new FormControl('', Validators.required);
+        this.password = new FormControl('', Validators.required);
+
+        this.registerForm = new FormGroup({
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email,
+            password: this.password
         });
     }
-  
-    cancel() {
-      this.router.navigate(['/']);
+
+
+    registerUser(user: IUser) {
+        this.saving = true;
+        this.saveAndRedirect(user);
     }
-  }
+
+    cancel() {
+        this.router.navigate(['/']);
+    }
+
+    private saveAndRedirect(user: IUser) {
+        this.userRepository.saveUser(user)
+            .subscribe({
+                error: () => this.saving = false,
+                complete: () => this.router.navigate(['/catalog'])
+            });
+    }
+}
