86603b6326d82337dc4cb6951620e3b242c98592
Ashutosh <Ashutosh>
2024-03-05 13:49:31 +0530

    Symbol naming best practices

--- Angular-Best-Practices/src/app/app.module.ts
+++ Angular-Best-Practices/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NavBarComponent } from './nav-bar.component';
 import { CatalogComponent } from "./catalog/catalog.component";
 import { RegisterComponent } from "./users/register.component";
 import { SignInComponent } from "./users/sign-in.component";
-import { LoadingComponent } from "./components/loading-spinner.component";
+import { LoadingSpinnerComponent } from "./components/loading-spinner.component";
 import { UserRepositoryService } from "./services/user-repository.service"
 import { AccountMenuComponent } from "./account-menu.component";
 import { CatalogRepositoryService } from './catalog/catalog-repository.service';
@@ -20,7 +20,7 @@ import { CatalogRepositoryService } from './catalog/catalog-repository.service';
     CatalogComponent,
     RegisterComponent,
     SignInComponent,
-    LoadingComponent,
+    LoadingSpinnerComponent,
     AccountMenuComponent
   ],
   imports: [
